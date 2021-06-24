ec9d8dcb73f5384857c599d77951762390c4f4a2
woori3104 <woori.kim.se@gmail.com>
2021-06-25 01:04:05 +0900

    return  type 변경

--- src/coffeeShop/searchCaffeeShop/searchCaffeShop.typeDefs.ts
+++ src/coffeeShop/searchCaffeeShop/searchCaffeShop.typeDefs.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
     type Query {
-        searchCaffeShop(keyword:String!, offset:Int): [User]
+        searchCaffeShop(keyword:String!, offset:Int): [CoffeeShop]
     }
 `;
