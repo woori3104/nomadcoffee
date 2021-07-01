94a1fda39eea1e3ad8d4cd78395c71a2a0af1b78
woori3104 <woori.kim.se@gmail.com>
2021-07-02 00:29:38 +0900

    resolver추가

--- src/coffeeShop/searchCoffeeShop/searchCoffeeShop.typeDefs.ts
+++ src/coffeeShop/searchCoffeeShop/searchCoffeeShop.typeDefs.ts
@@ -3,5 +3,6 @@ import { gql } from "apollo-server";
 export default gql`
     type Query {
         searchCoffeeShop(keyword:String!): [CoffeeShop]
+        searchCategories(keyword:String!): [CoffeeShop]
     }
 `;
